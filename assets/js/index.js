9af25c50ca5cfdcc2841cae267256145db67ea41
MarcoSavarin0 <ateneo2003@gmail.com>
2024-09-09 16:28:44 -0300

    vacunas seccion empezada, mobile terminada y tablet, desktop en progreso

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,4 +10,31 @@ document.addEventListener('DOMContentLoaded', () => {
             content.classList.add('open');
         }
     });
+    const buttons = document.querySelectorAll('.buttons__calendar');
+    const sections = document.querySelectorAll('.calendar');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            buttons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+            sections.forEach(section => {
+                section.classList.remove('show');
+            });
+            if (button.id === 'recien__nacidos') {
+                document.querySelector('.first__section__calendar__container__recienNacidos').classList.add('show');
+            } else if (button.id === 'anio_de_vida__button') {
+                document.querySelector('.first__section__calendar__container__añoDeVida').classList.add('show');
+            } else if (button.id === 'h_los_2_anios') {
+                document.querySelector('.first__section__calendar__container__hastaLos2Años').classList.add('show');
+            } else if (button.id === 'b_5_anios') {
+                document.querySelector('.first__section__calendar__container__5años').classList.add('show');
+            } else if (button.id === 'b_11_anios') {
+                document.querySelector('.first__section__calendar__container__11años').classList.add('show');
+            } else if (button.id === 'jovenes_adultos') {
+                document.querySelector('.first__section__calendar__container__Jovenes_adultos').classList.add('show');
+            } else if (button.id === 'embarazadas') {
+                document.querySelector('.first__section__calendar__container__embarazadas').classList.add('show');
+            }
+        });
+    });
 });
